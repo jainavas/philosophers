724c9bfc0cea7c89e9628527d30bf65fb69d40f9
jaime <jaimeipod99@gmail.com>
2024-11-03 22:14:15 +0100

    NOT NORMINETTE FRIENDLY

--- philo.c
+++ philo.c
@@ -6,36 +6,56 @@
 /*   By: jainavas <jainavas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 19:45:18 by jainavas          #+#    #+#             */
-/*   Updated: 2024/11/02 21:29:11 by jainavas         ###   ########.fr       */
+/*   Updated: 2024/11/03 22:12:52 by jainavas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_philo	*philonew(t_philo *philos, int num)
+void	freephilos(t_philo **philos, int nphilos)
+{
+	t_philo *tmp;
+
+	while (nphilos-- > 0)
+	{
+		tmp = (*philos)->right;
+		pthread_mutex_destroy(&(*philos)->lock);
+		free(*philos);
+		*philos = tmp;
+	}
+}
+
+void	philonew(t_philo **philos, int num)
 {
 	t_philo	*new;
+	struct timeval	tv;
 
 	new = malloc(sizeof(t_philo));
 	new->fork = 1;
-	new->right = NULL;
-	new->left = NULL;
-	new->philonum = num;
-	new->thread = NULL;
-	pthread_create(new->thread, NULL, philo, philos);
-	pthread_join(new->thread, NULL);
-	return (new);
-}
-
-void	philoadd(t_philo **lst, t_philo *new)
-{
-	if (*lst == NULL)
-		*lst = new;
+	gettimeofday(&tv, NULL);
+	new->alltime = tv.tv_usec;
+	if (!*philos)
+	{
+		new->left = NULL;
+		*philos = new;
+	}
 	else
 	{
-		new->left = *lst;
-		(*lst)->right = new;
+		new->left = philolast(*philos);
+		new->left->right = new;
 	}
+	new->philonum = num;
+	new->right = NULL;
+	pthread_mutex_init(&new->lock, NULL);
+}
+
+void	philoinit(t_philo *philo, char **argv)
+{
+	philo->timetodiems = ft_atoi(argv[2]);
+	philo->timetoeatms = ft_atoi(argv[3]);
+	philo->timetosleepms = ft_atoi(argv[4]);
+	philo->maxtimeseaten = ft_atoi(argv[5]);
+	pthread_create(&philo->thread, NULL, philo_routine, philo);
 }
 
 t_philo	*philolast(t_philo *lst)
@@ -49,27 +69,73 @@ t_philo	*philolast(t_philo *lst)
 	return (tmp);
 }
 
-void	philo(t_philo philos)
+void	makeround(int nphilos, t_philo **philos, char **argv)
 {
+	t_philo	*tmp;
+	int		n;
 	
+	tmp = *philos;
+	while (tmp->right)
+		tmp = tmp->right;
+	tmp->right = *philos;
+	(*philos)->left = tmp;
+	tmp = *philos;
+	n = 0;
+	while (nphilos > n++)
+	{
+		philoinit(tmp, argv);
+		tmp = tmp->right;
+	}
+	n = 0;
+	while (nphilos > n++)
+	{
+		pthread_join(tmp->thread, NULL);
+		tmp = tmp->right;
+	}
 }
 
-void *thread(void *arg)
+void	*philo_routine(t_philo *philo)
 {
-	printf("hola");
+	struct timeval	tv;
+	int				x;
+	int				c;
+
+	gettimeofday(&tv, NULL);
+	x = tv.tv_usec;
+	c = 0;
+	while (philo->timetodiems > (tv.tv_usec - x) / 1000
+		&& c < philo->maxtimeseaten)
+	{
+		x = tv.tv_usec;
+		printf("%ld %d is thinking\n",(tv.tv_usec)/1000, philo->philonum);
+		pthread_mutex_lock(&philo->lock);
+		pthread_mutex_lock(&philo->right->lock);
+		printf("%ld %d has taken a fork\n",(tv.tv_usec)/1000, philo->philonum);
+		usleep(philo->timetoeatms * 1000);
+		gettimeofday(&tv, NULL);
+		printf("%ld %d is eating\n",(tv.tv_usec)/1000, philo->philonum);
+		pthread_mutex_unlock(&philo->lock);
+		pthread_mutex_unlock(&philo->right->lock);
+		printf("%ld %d is sleeping\n",(tv.tv_usec)/1000, philo->philonum);
+		usleep(philo->timetosleepms * 1000);
+		gettimeofday(&tv, NULL);
+		c++;
+	}
+	printf("%ld %d died\n",(tv.tv_usec)/1000, philo->philonum);
 }
 
 int main(int argc, char **argv)
 {
-	pthread_t thread1;
-	t_vars		*vars;
-	int	x = 0;
-	int	numphilos = ft_atoi(argv[1]);
+	t_philo			*philos;
+	t_philo			*tmp;
+	int				x;
+	int				numphilos;
 
-	vars = malloc(sizeof(t_vars));
+	x = 0;
+	philos = NULL;
+	numphilos = ft_atoi(argv[1]);
 	while (x++ < numphilos)
-		philoadd(&vars->philos, philonew(vars->philos, x));
-	vars->philos->left = philolast(vars->philos);
-	vars->philos->left->right = vars->philos;
-	pthread_create(&thread1, NULL, thread, &x);
+		philonew(&philos, x);
+	makeround(numphilos, &philos, argv);
+	freephilos(&philos, numphilos);
 }
--- philo.h
+++ philo.h
@@ -6,35 +6,38 @@
 /*   By: jainavas <jainavas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:25:51 by jainavas          #+#    #+#             */
-/*   Updated: 2024/11/02 21:10:46 by jainavas         ###   ########.fr       */
+/*   Updated: 2024/11/03 21:20:00 by jainavas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# define _DEFAULT_SOURCE
+
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
 typedef struct philo
 {
 	int				fork;
 	int				philonum;
 	pthread_t 		thread;
+	pthread_mutex_t	lock;
 	struct philo	*right;
 	struct philo	*left;
+	int				timetodiems;
+	int				timetoeatms;
+	int				timetosleepms;
+	int				maxtimeseaten;
+	int				alltime;
 }	t_philo;
 
-typedef struct vars
-{
-	t_philo	*philos;
-	int		timetodiems;
-	int		timetoeatms;
-	int		timetosleepms;
-	int		maxtimeseaten;
-}	t_vars;
-
 int		ft_atoi(const char *str);
+t_philo	*philolast(t_philo *lst);
+void	*philo_routine(t_philo *philo);
 
 #endif
